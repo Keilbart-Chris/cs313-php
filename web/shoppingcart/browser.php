a5b67fd1ed5a4e4d0c5211b006611c9520242ba7
Chris Keilbart <chriskeilbart@gmail.com>
2017-09-30 20:55:18 -0700

    table attempt 1

--- web/shoppingcart/browser.php
+++ web/shoppingcart/browser.php
@@ -13,19 +13,35 @@
    <nav role="navigation" class="navfooter">
           <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/nav1.php'; ?>
    </nav>
-
-		<h2>Available Props:</h2><br>
-		
+   <h2 id="props">Available Props:</h2>
+   <table id="tab1">
+      <tr>
+         <th> </th>
+         <th> Image </th>
+         <th> Price </th>
+      </tr>
+      <tr>
+         <td>
+            <form method="post" action="viewcart.php" id="item1">
+            <a href="#" class="bton" onclick="document.getElementById('item1').submit()">Add to Cart</a>
+            </form>
+         </td>
+         <td>
+            <input type="checkbox" name="item[]" value="Pl" id="thing-pl"><label for = "thing-pl" >Plasma Grenade</label>
+         </td>
+         <td>
+            <p>$49.99</p>
+         </td>
       <img src="/images/plasma.jpg"  id="plasma"><br>
-		
+		<input type="checkbox" name="item[]" value="H" id="thing-h"><label for = "thing-h" >Helmet</label><br>
 		<img src="/images/spartanhelmet.jpg" id="helmet"><br>
-      
+      <input type="checkbox" name="item[]" value="Pi" id="thing-pi"><label for = "thing-pi" >Pistol</label><br>
 		<img src="/images/halopistol.jpg" id="pistol"><br>
-     
+      <input type="checkbox" name="item[]" value="C" id="thing-c"><label for = "thing-c" >Caster Gun</label><br>
 		<img src="/images/caster.jpg" id="caster"><br>
-      
+      <input type="checkbox" name="item[]" value="Bu" id="thing-bu"><label for = "thing-bu" >Buster Sword</label><br>
       <img src="/images/buster.jpg" id="buster"><br>
-      
+      <input type="checkbox" name="item[]" value="Ar" id="thing-ar"><label for = "thing-ar" >Assault Rifle</label><br>
 		<img src="/images/assaultrifle.jpg" id="ar"><br>
 		<input type="submit" name="SUBMIT">
 
