f67101a8e3dfcede3aead11298f38ba221313684
Chris Keilbart <chriskeilbart@gmail.com>
2017-09-30 17:39:32 -0700

    changed layout

--- web/shoppingcart/browser.php
+++ web/shoppingcart/browser.php
@@ -2,22 +2,39 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Browser</title>
+   <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/head.php'; ?>
 </head>
 <body>
-	<form method="post" action="viewcart.php">
-		
-		<label>Available Props:</label><br>
-		<input type="checkbox" name="item[]" value="Pl" id="thing-pl"><label for = "thing-pl" >Plasma Grenade</label><br>
-		<input type="checkbox" name="item[]" value="H" id="thing-h"><label for = "thing-h" >Helmet</label><br>
-		<input type="checkbox" name="item[]" value="Pi" id="thing-pi"><label for = "thing-pi" >Pistol</label><br>
-		<input type="checkbox" name="item[]" value="C" id="thing-c"><label for = "thing-c" >Caster Gun</label><br>
-		<input type="checkbox" name="item[]" value="Bu" id="thing-bu"><label for = "thing-bu" >Buster Sword</label><br>
-      <input type="checkbox" name="item[]" value="Ar" id="thing-ar"><label for = "thing-ar" >Assault Rifle</label><br>
-		
-		<input type="submit" name="ADD">
 
+   <header role="banner" id="header">
+      <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php'; ?>
+   </header>
+   <nav role="navigation" class="navfooter">
+          <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/nav1.php'; ?>
+   </nav>
 
-	</form>
+		<h2>Available Props:</h2><br>
+		
+      <img src="/images/plasma.jpg"  id="plasma"><br>
+		
+		<img src="/images/spartanhelmet.jpg" id="helmet"><br>
+      
+		<img src="/images/halopistol.jpg" id="pistol"><br>
+     
+		<img src="/images/caster.jpg" id="caster"><br>
+      
+      <img src="/images/buster.jpg" id="buster"><br>
+      
+		<img src="/images/assaultrifle.jpg" id="ar"><br>
+		<input type="submit" name="SUBMIT">
+
+   
+   <footer role="contentinfo" class="navfooter">
+      <div>
+         <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>
+         <?php echo date('j F, Y', getlastmod()) ?>
+      </div>
+   </footer>
 </body>
 </html>
