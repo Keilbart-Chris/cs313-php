341be1c55143bd2e293835f9d24998af6d138f97
Chris Keilbart <chriskeilbart@gmail.com>
2017-10-01 19:09:37 -0700

    changed post for checkout

--- web/shoppingcart/checkout.php
+++ web/shoppingcart/checkout.php
@@ -17,7 +17,7 @@ session_start();
           <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/nav1.php'; ?>
    </nav>
    <h4 id="bill">Billing Information:</h4>
-   <form method="post" action="checkout.php" id="datacollect">
+   <form method="post" action="confirmation.php" id="datacollect">
       <label>First Name</label><br>
       <input type="text" name="first" placeholder="first name"><br>
       <label>Last Name</label><br>
